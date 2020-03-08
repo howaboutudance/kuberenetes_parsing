9f4dc585dc2f0fce39bfad5d2efc951f91dd1611
Michael Penhallegon <mike@hematite.tech>
2020-03-08 16:08:07 -0700

    fixed parser

--- name_parse/parser.py
+++ name_parse/parser.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/parse/<filename>")
 def parse1Name(filename):
-    patt = re.compile("(?P<filename>(?P<vendor>[a-zA-Z0-9]+)_?"+
+    patt = re.compile("(?P<filename>(?P<vendor>[a-zA-Z0-9]+)_?" +
         "(?P<assay>A\d{3})?_?(?P<year>\d{4})?(?P<month>\d{2})"+
         "(?P<day>\d{2})?.*.(?P<filetype>\w{3}))")
     parsed = re.search(patt, filename).groupdict()
